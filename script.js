30223ded5f810a7cdb59388abb402275d266f7e8
Parnika Naik <naikparnika03@gmail.com>
2024-12-07 10:29:29 -0500

    commited

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+window.onload = () => {
+  alert("Welcome to my portfolio website!");
+};
